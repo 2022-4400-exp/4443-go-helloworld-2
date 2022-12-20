a576047de36cd3547632efc4681aa47e1fe2aacf
m19156 <m19156@g.metro-cit.ac.jp>
2022-12-20 14:21:23 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "4443: Messi is better than CR7 ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4443", "message": "Penaldo"})
+    }
+}
